8853aaaaf13301021218dc4dee07bbc658624f85
KangXinzhi <50164727+KangXinzhi@users.noreply.github.com>
2023-05-11 10:59:27 +0800

    feat: 备份

--- server/src/index copy.ts
+++ server/src/index copy.ts
@@ -10,7 +10,9 @@ import { AIChatMessage, HumanChatMessage } from "langchain/schema";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
 const chat = new ChatOpenAI({ temperature: 1 });
+
 const directoryPath = "src/files";
+
 const loadDocs = async (filePath: string) => {
   const extname = path.extname(filePath);
   let loader;
@@ -49,10 +51,10 @@ const loadAllDocs = async () => {
 let docsModal = await loadAllDocs();
 
 
-docsModal = docsModal.map(item => {
+docsModal = docsModal.map(item=>{
   // 单独处理pdf文件中的大量的\n
-  if (item?.metadata?.pdf) {
-    const newItem = { ...item };
+  if (item?.metadata?.pdf){
+    const newItem = {...item};
     newItem.pageContent = newItem.pageContent.replace(/\n/g, '');
     // console.log(newItem);
     return newItem;
@@ -60,40 +62,63 @@ docsModal = docsModal.map(item => {
   return item;
 });
 
-console.log('docsModal',docsModal);
 
 // Load the docs into the vector store
 const vectorStore = await HNSWLib.fromDocuments(docsModal, new OpenAIEmbeddings());
 
-const QUESTION = '我刚刚问了你哪些人呢？';
+
+const QUESTION = '课后服务的建议有哪些？';
 
 // 搜出相关的文档
-const result = await vectorStore.similaritySearchWithScore(QUESTION, 2);
+const result = await vectorStore.similaritySearchWithScore(QUESTION, 1);
 const docs = result.map(res => res[0]);
 
-console.log("docs", docs);
-
-const res = await chat.call([
-  new HumanChatMessage(
-    `Now, I will show you ${docs.length} context
+console.log("docs",docs);
+// eslint-disable-next-line consistent-return
+let positiveRes = await Promise.all(docs.map(async (doc) => {
+  const res = await chat.call([
+    new HumanChatMessage(
+      `Now, I will show you a context
     ------
-    ${docs.map((doc, index) => (
-      `${index + 1} context:
-       ${doc.pageContent}}
-      `
-    ))}
+    ${doc.pageContent}
     ------
     I'll give you a question.
-    with these context, I want you to tell me:
-    if the can answer the question please answer question 
-    if cannot answer the question please  answer question by you own knowledge
-    If I ask a question in Chinese, please answer it in Chinese. If I ask a question in English, please answer it in English.
-    Please answer the question directly, without stating the basis for the above
+    Ignoring all your previous knowledge, with these context, I want you to tell me:
+    zeta. can answer the question
+    theta. cannot answer the question
+    just return me the option value, don't add any extra text.
     `
+    ),
+    new AIChatMessage('Sure, please show me your question'),
+    new HumanChatMessage(QUESTION)
+  ]);
+  if (res.text.indexOf('zeta') !== -1 || (res.text.indexOf('theta') === -1 && res.text.indexOf('cannot') === -1)) {
+    return doc;
+  }
+}));
+
+positiveRes = positiveRes.filter(r => r);
+
+if (positiveRes.length === 0) {
+  console.log('没有在文档里找到相关资料');
+}
+
+// 将有用的资料拿去生成最终答案
+const finalRes = await chat.call([
+  new HumanChatMessage(
+    `
+Now, I will show you a context
+------
+${positiveRes.slice(0, 3).map(item => item?.pageContent).join('\n')}
+------
+I'll give you a question.
+Ignoring all your previous knowledge, with these context,
+If the context provided is irrelevant to the question asked. Just ignore the context and answer the question directly.
+If I ask a question in Chinese, please answer it in Chinese. If I ask a question in English, please answer it in English.
+Please answer the question
+       `
   ),
   new AIChatMessage('Sure, please show me your question'),
   new HumanChatMessage(QUESTION)
 ]);
-
-
-console.log(res);
+console.log(finalRes);
